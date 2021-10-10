97091bddb87d861825cd1ba93c9d6ec3ad37c013
Barongobber <barongobirn@gmail.com>
2021-10-10 19:12:31 +0800

    Deleting the unnecessary bootstraps

--- resources/views/client/layout/main.blade.php
+++ resources/views/client/layout/main.blade.php
@@ -14,8 +14,6 @@
   <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@100;200;300;400;700&family=Roboto:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <!-- BOOTSTRAP 4.6.0 -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
   <!-- CSS -->
   <link rel="stylesheet" href="{{asset('projectad/css/owl.carousel.min.css')}}">
