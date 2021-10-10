5ebd015654d6b982dc97ddd141f4062880f27e5e
Barongobber <barongobirn@gmail.com>
2021-10-11 01:13:05 +0800

    Deleting modal

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -103,32 +103,6 @@
          </div>
 
      </div>
-        <!-- Modal -->
-        {{-- <div class="modal fade" id="myModal" role="dialog">
-            <div class="modal-dialog">
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title"><Strong>Result</Strong></h4>
-                    <button onclick="login();" type="button" class="close" data-dismiss="modal">&times;</button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="swal2-icon swal2-success swal2-animate-success-icon" style="display: flex;">
-                            <div class="swal2-success-circular-line-left" style="background-color: rgb(255, 255, 255);"></div>
-                            <span class="swal2-success-line-tip"></span>
-                            <span class="swal2-success-line-long"></span>
-                            <div class="swal2-success-ring"></div>
-                            <div class="swal2-success-fix" style="background-color: rgb(255, 255, 255);"></div>
-                            <div class="swal2-success-circular-line-right" style="background-color: rgb(255, 255, 255);"></div>
-                        </div>
-                        <center class="success-login">Log in Success!!</center>
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" onclick="login();" class="btn btn-success" data-dismiss="modal">Ok</button>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
     </div>
     <script src="{{asset('js/jquery.min.js')}}"></script>
     <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
